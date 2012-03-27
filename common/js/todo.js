f45e909bc9173eefdea61158b8d5ac6e357e5f07
Andy Kant <andy@andykant.com>
2012-03-26 23:17:34 -0700

    Moved all 'common' files to the common directory. This includes views and the Todo/Todo.List models along with common js/css. Updated Can/jQuery accordingly.

---
+++ common/js/todo.js
@@ -0,0 +1,64 @@
+(function() {
+
+can.Model('Todo', {
+	localStore: function(cb){
+		var name = 'cantodo-jquery',
+			data = JSON.parse( window.localStorage[name] || (window.localStorage[name] = "{}") ),
+			res = cb.call(this, data);
+		if(res !== false){
+			for (var id in data) {
+				delete data[id].editing;
+			}
+			window.localStorage[name] = JSON.stringify(data);
+		}
+	},
+	findAll: function(params, success){
+		var def = new can.Deferred();
+		this.localStore(function(todos){
+			var instances = [];
+			for(var id in todos){
+				instances.push( new this(todos[id]) )
+			}
+			def.resolve({data: instances});
+		})
+		return def;
+	},
+	destroy: function(id, success){
+		var def = new can.Deferred();
+		this.localStore(function(todos){
+			delete todos[id];
+			def.resolve({});
+		});
+		return def
+	},
+	create: function(attrs, success){
+		var def = new can.Deferred();
+		this.localStore(function(todos){
+			attrs.id = attrs.id || parseInt(100000 *Math.random());
+			todos[attrs.id] = attrs;
+		});
+		def.resolve({id : attrs.id});
+		return def
+	},
+	update: function(id, attrs, success){
+		var def = new can.Deferred();
+		this.localStore(function(todos){
+			var todo = todos[id];
+			can.extend(todo, attrs);
+		});
+		def.resolve({});
+		return def
+	}
+},{});
+
+can.Model.List('Todo.List',{
+	
+	sort: function() {
+		return [].sort.call(this, function(a,b) {
+			return (a.text > b.text && 1) || (a.text < b.text && -1) || 0;
+		});
+	}
+	
+});
+
+})();
--- jquery/js/app.js
+++ jquery/js/app.js
@@ -1,66 +1,5 @@
 (function() {
 
-can.Model('Todo', {
-	localStore: function(cb){
-		var name = 'cantodo-jquery',
-			data = $.parseJSON( window.localStorage[name] || (window.localStorage[name] = "{}") ),
-			res = cb.call(this, data);
-		if(res !== false){
-			for (var id in data) {
-				delete data[id].editing;
-			}
-			window.localStorage[name] = JSON.stringify(data);
-		}
-	},
-	findAll: function(params, success){
-		var def = new can.Deferred();
-		this.localStore(function(todos){
-			var instances = [];
-			for(var id in todos){
-				instances.push( new this(todos[id]) )
-			}
-			def.resolve({data: instances});
-		})
-		return def;
-	},
-	destroy: function(id, success){
-		var def = new can.Deferred();
-		this.localStore(function(todos){
-			delete todos[id];
-			def.resolve({});
-		});
-		return def
-	},
-	create: function(attrs, success){
-		var def = new can.Deferred();
-		this.localStore(function(todos){
-			attrs.id = attrs.id || parseInt(100000 *Math.random());
-			todos[attrs.id] = attrs;
-		});
-		def.resolve({id : attrs.id});
-		return def
-	},
-	update: function(id, attrs, success){
-		var def = new can.Deferred();
-		this.localStore(function(todos){
-			var todo = todos[id];
-			can.extend(todo, attrs);
-		});
-		def.resolve({});
-		return def
-	}
-},{});
-
-can.Model.List('Todo.List',{
-	
-	sort: function() {
-		return [].sort.call(this, function(a,b) {
-			return (a.text > b.text && 1) || (a.text < b.text && -1) || 0;
-		});
-	}
-	
-});
-
 can.Control('Todos',{
 
 	// Initialize the Todos list
@@ -81,8 +20,8 @@ can.Control('Todos',{
 		Todo.findAll({}, function(todos) {
 			self.todos = todos.sort();
 			self.updateStats();
-			$('#todo-list').append(can.view('views/todos', { todos: self.todos }));
-			$('#stats').append(can.view('views/stats', { stats: self.stats }));
+			$('#todo-list').append(can.view('../common/views/todos', { todos: self.todos }));
+			$('#stats').append(can.view('../common/views/stats', { stats: self.stats }));
 		});
 	},
 		
@@ -100,7 +39,7 @@ can.Control('Todos',{
 	
 	// Handle a newly created Todo
 	"{Todo} created" : function(list, ev, item){
-		$('#todo-list').append(can.view('views/todo', { todo: item }));
+		$('#todo-list').append(can.view('../common/views/todo', { todo: item }));
 		this.updateStats();
 	},
 	
