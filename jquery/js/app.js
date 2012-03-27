099c982f95d53b89f51dd7eeda11a9cd20df7729
Andy Kant <andy@andykant.com>
2012-03-26 22:16:38 -0700

    Almost complete Can/jQuery demo, just need to add support for stats and clean up/reorganize things.

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -6,6 +6,9 @@ can.Model('Todo', {
 			data = $.parseJSON( window.localStorage[name] || (window.localStorage[name] = "{}") ),
 			res = cb.call(this, data);
 		if(res !== false){
+			for (var id in data) {
+				delete data[id].editing;
+			}
 			window.localStorage[name] = JSON.stringify(data);
 		}
 	},
@@ -14,7 +17,7 @@ can.Model('Todo', {
 		this.localStore(function(todos){
 			var instances = [];
 			for(var id in todos){
-				instances.push( new this( todos[id]) )
+				instances.push( new this(todos[id]) )
 			}
 			def.resolve({data: instances});
 		})
@@ -60,21 +63,21 @@ can.Model.List('Todo.List',{
 
 can.Control('Todos',{
 
+	// Initialize the Todos list
 	init : function(){
 		var self = this;
-		this.calendar = this.options.calendar;
 	
-		// empties the create input element
+		// Clear the new Todo entry
 		$("#new-todo").val('').focus();
 	
-		// fills this list of items (creates add events on the list)
+		// Render the Todos
 		Todo.findAll({}, function(todos) {
 			self.todos = todos.sort();
 			$('#todo-list').append(can.view('views/todos', { todos: todos }));
 		});
 	},
 		
-	// listens for key events and creates a new todo
+	// Listen for when a new Todo has been entered
 	"#new-todo keyup" : function(el, ev){
 		if(ev.keyCode == 13){
 			var todo = new Todo({
@@ -85,51 +88,58 @@ can.Control('Todos',{
 			});
 		}
 	},
-		
+	
+	// Handle a newly created Todo
 	"{Todo} created" : function(list, ev, item){
 		$('#todo-list').append(can.view('views/todo', { todo: item }))
 		 	
 		this.updateStats();
 	},
-		
-	"{Todo} destroyed" : function(list, ev, destroyed){
-		this.updateStats();
-	},
-				
-	"#clear-completed click" : function() {
-		can.each(this.todos, function(i, todo) {
-			if (todo.complete) {
-				todo.destroy();
-			}
+	
+	// Listen for editing a Todo
+	".todo dblclick" : function(el) {
+		var view = el.children('.view');
+		view.data('todo').attr('editing', true).save(function() {
+			view.children('.edit').focus();
 		});
 	},
+	
+	// Listen for an edited Todo
+	".todo .edit keyup" : function(el, ev){
+		if(ev.keyCode == 13){
+			el.closest('.todo').children('.view').data('todo')
+				.attr('editing', false)
+				.attr('text', el.val())
+				.save();
+		}
+	},
+	
+	// Listen for the toggled completion of a Todo
+	".todo .toggle change" : function(el, ev) {
+		el.closest('.view').data('todo')
+			.attr('complete', el.is(':checked'))
+			.save();
+	},
 		
+	// Handle an updated Todo
+	"{Todo} updated" : function(list, ev, item){
+		this.updateStats();
+	},
+	
+	// Listen for a removed Todo
 	".todo .destroy click" : function(el){;
 		el.closest('.view').data('todo').destroy();
 	},
-		
-	".todo .toggle change" : function(el, ev) {
-		var todo = el.closest('.view').data('todo');
-		todo.attr('complete', el.is(':checked')).save();
+	
+	// Listen for removing all completed Todos
+	"#clear-completed click" : function() {
+		for (var i = this.todos.length - 1, todo; i > -1 && (todo = this.todos[i]); i--) {
+			todo.attr('complete') && todo.destroy();
+		}
 	},
-		
-	// // switch to edit mode
-	// ".todo dblclick" : function(el){
-	// 	var input = can.$("<input name='text' class='text'/>").set('value', el.getData('todo').text)
-	// 	el.setContent(input);
-	// 	input.focus();
-	// },
-	// 	
-	// // update the todo's text on blur
-	// ".todo [name=text] focusout" : function(el, ev){
-	// 
-	// 	var todo = el.ancestor('.todo').getData('todo').update({
-	// 		text : el.get('value')
-	// 	});
-	// },
-	// 	
-	// when an item is updated
-	"{Todo} updated" : function(list, ev, item){
+	
+	// Handle a destroyed Todo
+	"{Todo} destroyed" : function(list, ev, destroyed){
 		this.updateStats();
 	},
 		
