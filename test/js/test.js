0c4dd0776b083344c5d478cb40ac56a12c74c2ee
Andy Kant <andy@andykant.com>
2012-03-28 12:56:19 -0700

    Updated initial tests

--- test/js/test.js
+++ test/js/test.js
@@ -4,7 +4,7 @@ steal('funcunit', 'funcunit/qunit').then(function() {
 	var oldmodule = window.module;
 	window.module = function(name, testEnvironment) {
 		oldmodule(TODOLIB + '/' + name, testEnvironment);
-	}
+	};
 	
 	module('js/todo.js', {
 		setup: function() {
@@ -23,17 +23,48 @@ steal('funcunit', 'funcunit/qunit').then(function() {
 			});
 		}
 	});
-		
-	test('Add a todo', function() {
-		S('#new-todo').type('new todo\r');
-		S('#todo-list .todo:last-child label').text('new todo', 'has new todo');
-	});
 	
-	test('Edit a todo', function() {
-		S('#new-todo').type('new todo\r');
-		S('#todo-list .todo:last-child').dblclick();
-		S('#todo-list .todo:last-child .edit').type('edited todo\r');
-		S('#todo-list .todo:last-child label').text('edited todo', 'has edited todo');
+	// Helpers for easily modifying todos
+	var helpers = {
+		add: function(nthChild, text) {
+			S('#new-todo').type(text + '\r');
+			S('#todo-list .todo:nth-child('+nthChild+') label').text(text, 'added a todo');
+		},
+		
+		edit: function(nthChild, text) {
+			S('#todo-list .todo:nth-child('+nthChild+')').dblclick();
+			S('#todo-list .todo:nth-child('+nthChild+') .edit').type(text + '\r');
+			S('#todo-list .todo:nth-child('+nthChild+') label').text(text, 'edited a todo');
+		},
+		
+		complete: function(nthChild) {
+			S('#todo-list .todo:nth-child('+nthChild+') .toggle').then(function(el) {
+				el.prop('checked', false);
+			});
+			S('#todo-list .todo:nth-child('+nthChild+') .toggle').click();
+			S('#todo-list .todo:nth-child('+nthChild+')').hasClass('done', true, 'completed a todo');
+		},
+		
+		incomplete: function(nthChild) {
+			S('#todo-list .todo:nth-child('+nthChild+') .toggle').then(function(el) {
+				el.prop('checked', true);
+			});
+			S('#todo-list .todo:nth-child('+nthChild+') .toggle').click();
+			S('#todo-list .todo:nth-child('+nthChild+')').hasClass('done', false, 'incompleted a todo');
+		},
+		
+		remove: function(nthChild) {
+			S('#todo-list .todo:nth-child('+nthChild+') .destroy').click();
+			S('#todo-list .todo:nth-child('+nthChild+')').missing('remove a todo');
+		}
+	};
+		
+	test('Add, edit, complete, incomplete, and remove a todo', function() {
+		helpers.add(1, 'new todo');
+		helpers.edit(1, 'edited todo');
+		helpers.complete(1);
+		helpers.incomplete(1);
+		helpers.remove(1);
 	});
 	
 });
