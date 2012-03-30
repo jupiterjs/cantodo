613edd03a1ded4eac24a4bf7dbd5cd3b0456f299
Andy Kant <andy@andykant.com>
2012-03-29 19:13:07 -0700

    Updating the edit blur test

--- test/js/test.js
+++ test/js/test.js
@@ -80,6 +80,9 @@ steal('funcunit', 'funcunit/qunit').then(function() {
 		helpers.add(1, 'new todo');
 		S('#todo-list .todo:nth-child(1)').dblclick();
 		S('#todo-list .todo:nth-child(1) .edit').type('more edits\t');
+		S('#todo-list .todo:nth-child(1) .edit').then(function(el) {
+			el[0].blur();
+		});
 		S('#todo-list .todo:nth-child(1) label').text('more edits', 'edited a todo (blur)');
 	});
 		
